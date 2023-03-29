7ee1c43af8aad24493c17ca4e906c5995db52968
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-03-29 12:18:14 +0530

    Initial Project Commit

---
+++ src/test/java/com/m2p/ArithmeticOperationTest.java
@@ -0,0 +1,12 @@
+package com.m2p;
+
+import org.junit.jupiter.api.Test;
+
+public class ArithmeticOperationTest {
+
+    @Test
+    void toReturnNineForFiveAndFourAsInput()
+    {
+        ArithmeticOperation arithmeticObject = new ArithmeticOperation();
+    }
+}
