56b48a2949ec3a65df1a0c7ee8d9804d0fad5e2d
naveenmr2001 <naveen.mr@m2pfintech.com>
2023-03-29 12:33:26 +0530

    Add. Add the testCase to Addition

--- src/main/java/com/m2p/ArithmeticOperation.java
+++ src/main/java/com/m2p/ArithmeticOperation.java
@@ -1,7 +1,20 @@
 package com.m2p;
 
 public class ArithmeticOperation {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public int Add(int firstInputNumber,int secondInputNumber){
+        return firstInputNumber+secondInputNumber;
     }
+
+    public int Subtract(int firstInputNumber,int secondInputNumber){
+        return firstInputNumber-secondInputNumber;
+    }
+
+    public int Multiplication(int firstInputNumber,int secondInputNumber){
+        return firstInputNumber*secondInputNumber;
+    }
+
+    public int Divison(int firstInputNumber,int secondInputNumber){
+        return firstInputNumber/secondInputNumber;
+    }
+
 }
--- src/test/java/com/m2p/ArithmeticOperationTest.java
+++ src/test/java/com/m2p/ArithmeticOperationTest.java
@@ -2,11 +2,18 @@ package com.m2p;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ArithmeticOperationTest {
 
     @Test
     void toReturnNineForFiveAndFourAsInput()
     {
         ArithmeticOperation arithmeticObject = new ArithmeticOperation();
+        int exceptedValue = 9;
+
+        int actualValue = arithmeticObject.Add(5,4);
+
+        assertEquals(exceptedValue,actualValue);
     }
 }
